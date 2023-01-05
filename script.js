bbf6197d2a4b55689a0a85c9c92eb184727a98c4
LuckyBlankBunny <doublescreen50@gmail.com>
2023-01-05 20:30:30 +0200

    initial changes

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ window.addEventListener('DOMContentLoaded', () => {
                             <img src="images/inhabitants/iconInhabitants.png" alt="iconInhabitants">
                         </li>
                         <li>
-                            <input type="number" name="c${this.title}" value="0" class="cText cTextwidth saveinput inhabitantInput" maxlength="6" tabindex="1" alt="">
+                            <input type="number" name="c${this.title}" value=0 placeholder=0 class="cText cTextwidth saveinput inhabitantInput" maxlength="6" tabindex="1" alt="">
                         </li>
                     </ul>
                 </li>
@@ -50,7 +50,7 @@ window.addEventListener('DOMContentLoaded', () => {
                     <ul>
                         <li><input type="radio" name="c${this.title}Radio" class="houseRadio" value="House" alt=""></li>
                         <li><img src="images/inhabitants/iconHouse.png" height="22" width="22" alt=""></li>
-                        <li><input type="text" name="c${this.title}House" value="0" class="cText cTextwidth saveinput houseInput" maxlength="6" alt="" style="color: rgb(10, 10, 10); background-color: rgb(241, 241, 241);" disabled></li>
+                        <li><input type="text" name="c${this.title}House" value=0 placeholder=0 class="cText cTextwidth saveinput houseInput" maxlength="6" alt="" style="color: rgb(10, 10, 10); background-color: rgb(241, 241, 241);" disabled></li>
                     </ul>
                 </li>
             `;
@@ -67,10 +67,11 @@ window.addEventListener('DOMContentLoaded', () => {
           allInhabitantsInputs = inhabitants.querySelectorAll('.inhabitantInput'),
           allHouses = inhabitants.querySelectorAll('.houseRadio'),
           allHousesInputs = inhabitants.querySelectorAll('.houseInput');
-        
+
     mainSwitcher.forEach(item => {
         item.addEventListener('click', (e) => {
             if  (e.target.value == "inhabitants") {
+
                 allInhabitants.forEach(item => {
                     item.checked = true;
                 })
@@ -88,6 +89,7 @@ window.addEventListener('DOMContentLoaded', () => {
                     item.checked = false;
                 })
             } else {
+
                 allHouses.forEach(item => {
                     item.checked = true;
                 })
@@ -105,6 +107,72 @@ window.addEventListener('DOMContentLoaded', () => {
                 })
             }
         })
+    })
+
+    //individual switcher
+
+    function inputSettings(nameRadio, nameInhabitant, nameHouse) {
+        const cNameRadio = document.querySelectorAll(`[name="${nameRadio}"]`),
+              cNameHouse = document.querySelector(`[name="${nameHouse}"]`),
+              cNameInhabitant = document.querySelector(`[name="${nameInhabitant}"]`);
+
+        cNameHouse.setAttribute("disabled", "disabled");
+        cNameHouse.style.cssText = 'color: #0A0A0A; background-color: #F1F1F1';
+
+        cNameRadio.forEach(item => {
+            item.addEventListener('change', () => {
+                if (item.checked && item.value == 'inhabitants') {
+                    cNameHouse.setAttribute("disabled", "disabled");
+                    cNameInhabitant.removeAttribute("disabled");
+                    cNameHouse.style.cssText = 'color: #0A0A0A; background-color: #F1F1F1';
+                    cNameInhabitant.style.cssText = 'color: #000; background-color: #FFFFFF';
+                } else {
+                    cNameInhabitant.setAttribute('disabled', "disabled");
+                    cNameHouse.removeAttribute('disabled');
+                    cNameInhabitant.style.cssText = 'color: #0A0A0A; background-color: #F1F1F1';
+                    cNameHouse.style.cssText = 'color: #000; background-color: #FFFFFF';
+                }
+            })
+        })
+    }
+
+    function inputSettingsAll() {
+        for (let key of inhabitantsDB) {
+            inputSettings(`c${key["title"]}Radio`, `c${key["title"]}`, `c${key["title"]}House`)
+        }
+    }
+    
+    inputSettingsAll();
+
+    // top buttons
+
+    const topButtons = document.querySelectorAll('.nav ol li'),
+          topButtonsParent = document.querySelector('.nav ol');
+
+    function removeActiveButton() {
+        topButtons.forEach(item => {
+            item.classList.remove('activelink');
+        })
+    }
+
+    function addActiveButton(i = 0) {
+        topButtons[i].classList.add('activelink')
+    }
+
+    removeActiveButton()
+    addActiveButton()
+
+    topButtonsParent.addEventListener('click', event => {
+        const target = event.target;
+        console.log(target)
+        if(target && target.classList.contains("top-buttons")) {
+            topButtons.forEach((btn, i) => {
+                if(target == btn) {
+                    removeActiveButton()
+                    addActiveButton(i)
+                }
+            })
+        }
     })
 
     //table
